e6c228df0ffe148b840ed13368bae2dfed91fe59
hgungor0694@gmail.com <hgungor0694@gmail.com>
2019-06-16 16:08:25 +0300

    Ürün silme geliştirildi. Bilgilendirme göstermek için toastr ve IslemSonuc sınıfı eklendi.

---
+++ StokTakip.Entities/Other/Enumarations.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StokTakip.Entities.Other
+{
+    public static class IslemSonucTur
+    {
+        public const int Basarili = 1;
+        public const int Hatali = 2;
+    }
+}
